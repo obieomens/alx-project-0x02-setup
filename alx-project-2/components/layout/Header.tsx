e17975dce46172d151ff02f4ebec14a243088178
obie <obieomens@gmail.com>
2025-07-29 11:26:58 +0100

    added homepage and about

--- alx-project-2/components/layout/Header.tsx
+++ alx-project-2/components/layout/Header.tsx
@@ -1,6 +1,13 @@
+import Link from "next/link";
+
 const Header: React.FC = () =>{
     return (
-        <div></div>
+    <header style={{ padding: '1rem', backgroundColor: '#f0f0f0'}}>
+        <nav style={{ display: 'flex', gap: '1rem' }}>
+            <Link href="/home">Home</Link>
+            <Link href="/about">About</Link>
+        </nav>
+    </header>
     )
 }
 export default Header;
---
+++ alx-project-2/pages/about.tsx
@@ -0,0 +1,8 @@
+const About: React.FC = () => {
+    return (
+        <div>
+            <h1>About Us page</h1>
+        </div>
+    )
+}
+export default About;
